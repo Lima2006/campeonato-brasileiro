009b759f164b05e90f96faafa17ae40c1f1723e4
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-08-05 14:27:11 -0300

    feat: type components

--- src/components/features/championship-table/components/players-tab-body.tsx
+++ src/components/features/championship-table/components/players-tab-body.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import { Column } from "components/toolkit";
 import useGetPlayers from "../hooks/use-get-players";
 import PlayerCard from "./player-card";
 
@@ -9,7 +8,7 @@ const PlayersTabBody: React.FC<PlayersTabBodyProps> = ({}) => {
   if (!data) return null;
   return (
     <div className="grid grid-cols-6 gap-2 p-2 shadow-gray-300 mx-auto">
-      {data.map((item: any) => (
+      {data.map((item) => (
         <PlayerCard key={item.atleta.atleta_id} playerData={item} />
       ))}
     </div>
